76d3bdfe0dc5f9a5c61e488b57c8bc589dc6ad1d
ider <326737833@qq.com>
2022-09-14 15:14:08 +0800

    future: better note commit logic

--- src/components/NoteBook.vue
+++ src/components/NoteBook.vue
@@ -7,7 +7,7 @@ el-container
         el-button(type="primary",v-if="!viewStatus", @click="viewStatus=!viewStatus",style="margin-top: -40px;") 编辑 Note
         el-button(type="success",v-else,@click="viewStatus=!viewStatus",style="margin-top: -40px;") 展示 Note
     el-row.md-editor(v-loading="loading")
-      md-editor(v-model="text",v-if="viewStatus",:htmlPreview="false",:toolbarsExclude="toolbarsExclude",:onSave="saveNoteBook",editorId="edit",:previewOnly="!father.editMode")
+      md-editor(v-model="text",v-if="viewStatus",:htmlPreview="false",:toolbarsExclude="toolbarsExclude",@change="onChange",@save="saveNoteBook",editorId="edit",:previewOnly="!father.editMode")
       md-editor(v-model="text",v-else,:htmlPreview="false",editorId="view",:previewOnly="true")
 </template>
 
@@ -18,42 +18,64 @@ export default {
 </script>
 
 <script lang="ts" setup>
-import { ref, defineProps, onMounted } from "vue";
+import { ref, defineProps, onMounted, onUnmounted } from "vue";
 import MdEditor from "md-editor-v3";
 import { ElMessage } from "element-plus";
-import { apiSeveice } from "@/utils/requests";
-
-console.log(MdEditor.defToolbars);
+import { getNote, setNote } from "@/utils/requests";
 
 const toolbarsExclude = ["github", "htmlPreview", "image"];
+let noteID = "";
 
 const saveNoteBook = async (data: string) => {
-  await apiSeveice.put(`v/1.0/fresh/notebok/${father.storagekey}`, {
-    data: data,
-  });
-  ElMessage({
-    message: "保存成功",
-    type: "success",
-  });
+  try {
+    const rep = await setNote(father.storagekey, noteID, data);
+    ElMessage({
+      message: "保存成功",
+      type: "success",
+    });
+  } catch (error) {
+    ElMessage({
+      message: `保存失败,修改的页面并非最新版本,请刷新后重新修改: ${error}`,
+      type: "warning",
+    });
+  }
+  modified.value = true;
 };
 
 const text = ref("");
 const viewStatus = ref(false);
 const loading = ref(true);
+const modified = ref(false);
 
-const getNoteBook = () => {
-  apiSeveice
-    .get(`v/1.0/fresh/notebok/${father.storagekey}`)
-    .then((Response) => {
-      text.value = Response.data.data;
-    })
-    .finally(() => {
-      loading.value = false;
-    });
+const onChange = (txt: string) => {
+  text.value = txt;
+  modified.value = true;
+};
+
+const getNoteBook = async () => {
+  let dataObj = await getNote(father.storagekey);
+  console.log(dataObj);
+  text.value = dataObj.data;
+  noteID = dataObj.id;
+  loading.value = false;
 };
 
 onMounted(() => {
   getNoteBook();
+  window.onbeforeunload = (e) => {
+    if (!modified.value) {
+      return;
+    }
+    // 通知浏览器不要执行与事件关联的默认动作
+    e.preventDefault();
+    // Chrome 需要 returnValue 被设置成空字符串
+    e.returnValue = "";
+  };
+});
+
+onUnmounted(() => {
+  // 取消对事件的监听
+  window.onbeforeunload = null;
 });
 
 const father = defineProps({
@@ -66,6 +88,7 @@ const father = defineProps({
     default: true,
   },
 });
+
 // const text = ref("# Hello Editor " + father.storagekey);
 </script>
 
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,17 +1,45 @@
 import axios from "axios";
 
 // 创建axios实例
-const localSeveice = axios.create({
-  timeout: 150000000, // 请求超时时间
+export const localSeveice = axios.create({
+  timeout: 120 * 1000,
 });
-
 localSeveice.defaults.baseURL = "/v2";
 
 // 创建axios实例
-const apiSeveice = axios.create({
-  timeout: 150000000, // 请求超时时间
+export const apiSeveice = axios.create({
+  timeout: 120 * 1000,
 });
-
 apiSeveice.defaults.baseURL = "https://api.nikepai.com";
 
-export { localSeveice, apiSeveice };
+export const getfresh = async (path: string, key: string) => {
+  return await apiSeveice.get(`v/1.0/fresh/${path}/${key}`);
+};
+
+export const setfresh = async (
+  path: string,
+  key: string,
+  id: string,
+  data: string
+) => {
+  return apiSeveice.post(`v/1.0/fresh/${path}/${key}`, {
+    id,
+    data: data,
+  });
+};
+
+export const getNote = async (key: string) => {
+  const rep = await getfresh("notebok", key);
+  if (rep.data.code === 200) {
+    return rep.data.data;
+  }
+  throw Error(rep.data.message || rep.status);
+};
+
+export const setNote = async (key: string, id: string, data: string) => {
+  const rep = await setfresh("notebok", key, id, data);
+  if (rep.data.code === 200) {
+    return rep.data;
+  }
+  throw Error(rep.data.message || rep.status);
+};
