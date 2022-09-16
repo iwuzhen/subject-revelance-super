b6278595b123ca949df04d393e07bb076dc45524
ider <326737833@qq.com>
2022-09-16 10:59:08 +0800

    future: paper search

--- src/views/demo/mag/fos-trend-2022.vue
+++ src/views/demo/mag/fos-trend-2022.vue
@@ -1,11 +1,32 @@
 <template lang="pug">
+el-dialog(v-model="dialogTableVisible" title="Paper information" width="90%")
+  el-space(wrap)
+    el-card(v-for="item in paperItem" :to="item.href" target="_blank")
+      template(#header)
+        .card-header
+          span.text-3xl {{item.title}}
+          br
+          span.zh {{ en2zhdict[item.title]===undefined?'Loading...': en2zhdict[item.title] }}
+      description(v-if="item.publisher !==''")
+        p publisher: {{item.publisher}}
+
+      el-row
+        .author( v-for="author in item.authors" :key="author.name" )
+          .name {{author.name}}
+          .org  {{author.org}}
+      .tag
+        time.italic {{item.year}}
+
+      .tag
+        el-tag( v-for="tag in item.fos"  :key="tag.name"  :disable-transitions="false" ) {{tag.name}}
+
 el-row
-  el-col(:span="3")
+  el-col(:span="6")
     el-form-item(label="type:" size="large")
       el-select(v-model="appStore.states.typeSelect",placeholder="图表选择",style="width: 100%",size='large',@change='updateChart')
         el-option(v-for="item in typeOpt",:key="item",:label="item",:value="item")
 
-  el-col(:span="3")
+  el-col(:span="6")
     el-form-item(label="最小值过滤:" size="large")
       el-input-number(v-model="appStore.states.yz" :step="1" :min="1",@change='updateChart')
 
@@ -13,14 +34,28 @@ el-row
     el-form-item(label="选中的学科 :" size="large")
       el-select(v-model="appStore.states.subjectSelect",allow-create,multiple,filterable,size='large',@change='updateChart')
         el-option(v-for="item in subjectOpt",:key="item",:label="item",:value="item")    
+el-row
+  el-col(:span="8") 
+    el-form-item(label="搜索:" size="large")
+      el-autocomplete(v-model="queryName"
+      :fetch-suggestions="querySearchAsync"
+      clearable
+      placeholder="Please input"
+      @select="handleSelect")
+  el-col(:span="6")
+    el-switch(@change="reloadChange",v-model="queryFlag",size="large",active-text="子查询",inactive-text="父查询")
+  el-col(:span="4")
+    el-button(type="primary" @click="initSubject") 回到顶级学科
 
+el-row
   el-col(:span="12")
-    el-tree(:props="props" lazy :load="loadNode" show-checkbox :check-strictly="true" @check-change="handleCheckChange",node-key="name",accordion,:default-checked-keys="appStore.states.subjectSelect")
+    el-tree(v-if="treeFlag" :props="props" lazy :load="loadNode" show-checkbox :check-strictly="true" @check-change="handleCheckChange",node-key="name",accordion,:default-checked-keys="appStore.states.subjectSelect")
       template.custom-tree-node(v-slot:="{ node }") {{ node.data.name }}
         el-tag(class="ml-2") {{ en2zhdict[node.data.name]===undefined?'Loading...': en2zhdict[node.data.name] }}
         el-tag(class="ml-2" type="success" v-if="node.data.size>0") {{node.data.size}}
   
 el-row
+  el-alert(title="点击 chart 中的点可以查询其中的文章" type="info" ) 
   el-col(:span="24")
     #echart1.echart
 </template>
@@ -43,6 +78,7 @@ import { reactive, ref, onMounted } from "vue";
 import _ from "lodash";
 import * as echarts from "echarts";
 import { extendEchartsOpts } from "@/utils/model";
+import { ElMessage } from "element-plus";
 
 const appHomeStore = homeStore();
 appHomeStore.title = "MAG FOS trend";
@@ -70,11 +106,59 @@ const ParentCategoriesPath =
 const GetFosByNamePath =
   "https://wiki.lmd.knogen.com:10443/api/mag/getFosByName";
 const queryFlag = ref(true);
+const dialogTableVisible = ref(false);
 
 const props = {
   label: "name",
   children: "zones",
   isLeaf: "leaf",
+};
+interface LinkItem {
+  value: string;
+  id: number;
+}
+let queryObj = reactive({
+  name: "",
+  value: 0,
+});
+
+const treeFlag = ref(true);
+const queryName = ref("");
+const querySearchAsync = async (
+  queryString: string,
+  cb: (arg: any) => void
+) => {
+  let response = await axios.request({
+    url: GetFosByNamePath,
+    method: "post",
+    data: {
+      name: queryString,
+    },
+  });
+  const results = response.data.data.map((item: { name: any; id: any }) => {
+    return {
+      value: item.name,
+      id: item.id,
+    };
+  });
+  cb(results);
+};
+const handleSelect = (item: LinkItem) => {
+  queryObj.value = item.id;
+  queryObj.name = item.value;
+  reloadChange();
+};
+
+const reloadChange = () => {
+  treeFlag.value = false;
+  setTimeout(() => {
+    treeFlag.value = true;
+  }, 5);
+};
+
+const initSubject = () => {
+  queryObj.value = 0;
+  reloadChange();
 };
 
 const subjectOpt: string[] = [];
@@ -94,7 +178,47 @@ onMounted(() => {
       element.resize();
     });
   }, 500);
+
+  myChartObjs[0].on("click", "series.line", function (params: any) {
+    console.log(params);
+    if (params.value[params.encode.y[0]] > 500) {
+      ElMessage({
+        message: `超过500篇的文章查询没有成功`,
+        type: "warning",
+      });
+      return;
+    }
+
+    getPaperDetail(params.seriesName.toLowerCase(), params.data[0]);
+  });
 });
+
+// FOS某各类在year中发表的论文列表
+// https://wiki.lmd.knogen.com:10443/api/mag/getMagInfoByFos
+// {
+//     "cat":"mechanics",//单选
+//     "year":1851, //年份
+//     "type":"article" //[article,book,patent]
+// }
+let paperItem = ref<any[]>([]);
+const getPaperDetail = async (name: string, year: number) => {
+  let url = "https://wiki.lmd.knogen.com:10443/api/mag/getMagInfoByFos";
+  let response = await axios.request({
+    url,
+    method: "post",
+    data: {
+      type: appStore.states.typeSelect,
+      cat: name,
+      year: year,
+    },
+  });
+  paperItem.value = response.data.data;
+  dialogTableVisible.value = true;
+  for (let item of response.data.data) {
+    addTranslateChan(item.title);
+  }
+  console.log(response.data);
+};
 
 const updateChart = _.debounce(async () => {
   let url = "https://wiki.lmd.knogen.com:10443/api/mag/getFosCountByYear";
@@ -114,11 +238,13 @@ const updateChart = _.debounce(async () => {
   let dataObj = response.data.data;
   let dataset = [["year", ...Object.keys(dataObj)]];
   let dataFlag = false;
+  let fosFirstYear = {};
   for (let year = 1800; year <= 2020; year++) {
     let row: any[] = [year];
     for (let key of dataset[0].slice(1)) {
       row.push(dataObj[key][year] || "-");
       if (dataObj[key][year]) {
+        if (!fosFirstYear[key]) fosFirstYear[key] = [year, dataObj[key][year]];
         dataFlag = true;
       }
     }
@@ -130,7 +256,7 @@ const updateChart = _.debounce(async () => {
     appStore.states.subjectSelect,
     dataset.slice(-1)[0].slice(1)
   );
-
+  console.log("fosFirstYear", fosFirstYear);
   fuseData.sort(
     (x, y) => (y[1] as unknown as number) - (x[1] as unknown as number)
   );
@@ -169,14 +295,6 @@ const updateChart = _.debounce(async () => {
     ],
     tooltip: {
       formatter: function (params: any) {
-        params.sort((x: any, y: any) => {
-          let dataIDX: number = x.encode.y[0];
-          let dataIDY: number = y.encode.y[0];
-          // if (y.data === undefined) {
-          //   return -1;
-          // }
-          return y.data[dataIDY] - x.data[dataIDX];
-        });
         let showHtm = ` ${params[0].name}<br>`;
         for (let i = 0; i < params.length; i++) {
           let dataID: number = params[i].encode.y[0];
@@ -189,7 +307,7 @@ const updateChart = _.debounce(async () => {
           if (_text.includes("占比")) {
             _data = _.round(_data * 100, 4) + "%";
           }
-          showHtm += `${_marker}${_text}：${_data}<br>`;
+          showHtm += `${_marker}${_text}:${_data}<br>`;
         }
         return showHtm;
       },
@@ -199,6 +317,17 @@ const updateChart = _.debounce(async () => {
         datasetIndex: 0,
         type: "line",
         name: item,
+        markPoint: {
+          data: [
+            {
+              // type: "min",
+              name: "Min",
+              xAxis: fosFirstYear[item.toLowerCase()][0],
+              value: fosFirstYear[item.toLowerCase()][1],
+              yAxis: fosFirstYear[item.toLowerCase()][1],
+            },
+          ],
+        },
         encode: {
           x: "year",
           y: item.toLowerCase(),
@@ -227,7 +356,19 @@ const handleCheckChange = (
 const loadNode = async (node: Node, resolve: (data: Tree[]) => void) => {
   if (node.level === 0) {
     let response;
-    response = await axios.get(OriginCategoriesPath);
+    if (queryObj.value > 0) {
+      addTranslateChan(queryObj.name);
+      return resolve([
+        {
+          id: queryObj.value,
+          name: queryObj.name,
+          size: 0,
+          leaf: false,
+        },
+      ]);
+    } else {
+      response = await axios.get(OriginCategoriesPath);
+    }
     let root: Tree[] = response.data.data.map(
       (item: { id: any; name: any; size: number }) => {
         addTranslateChan(item.name);
@@ -239,9 +380,8 @@ const loadNode = async (node: Node, resolve: (data: Tree[]) => void) => {
         };
       }
     );
-
     return resolve(root);
-  } else if (node.level == 1) {
+  } else if (node.level > 0) {
     let response = await axios.request({
       url: queryFlag.value ? ChildCategoriesPath : ParentCategoriesPath,
       method: "post",
@@ -256,14 +396,12 @@ const loadNode = async (node: Node, resolve: (data: Tree[]) => void) => {
           id: item.id,
           name: item.name,
           size: item.size,
-          leaf: true,
+          leaf: item.size === 0 ? true : false,
         };
       }
     );
     // console.log("root", root);
     return resolve(root);
-  } else if (node.level > 1) {
-    return resolve([]);
   }
 };
 
@@ -289,11 +427,37 @@ const addTranslateChan = (rst: string) => {
 };
 </script>
 
-<style lang="less">
-.el-tag {
-  margin-left: 20px;
-}
+<style lang="less" scoped>
 .el-select {
   width: 95%;
+}
+.el-card {
+  width: 20rem;
+  text-align: end;
+  .el-row {
+    margin: 0;
+    .author {
+      margin-right: 10px;
+      .name {
+        white-space: nowrap;
+        font-style: normal;
+      }
+      .org {
+        white-space: nowrap;
+        font-style: italic;
+      }
+    }
+  }
+  .card-header {
+    text-align: end;
+    .zh {
+      font-style: italic;
+    }
+  }
+  .tag {
+    .el-tag {
+      margin-left: 5px;
+    }
+  }
 }
 </style>
